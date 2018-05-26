3b05dfa38be918225f89d1fa45515f21b0b49a8d
cmd16 <catherine.dejager10@gmail.com>
2018-05-26 00:39:49 -0400

    Added damage type as an instance variable of Attack

--- attack_class.py
+++ attack_class.py
@@ -1,14 +1,19 @@
 from utility_methods_dnd import roll_dice, validate_dice
 
 class Attack:
-    def __init__(self, damage_dice, attack_mod=0, damage_mod=0, name=""):
-        self._attack_mod = attack_mod
+    def __init__(self, damage_dice, attack_mod=0, damage_mod=0, damage_type="", name=""):
         if not isinstance(attack_mod, int):
             raise ValueError("Attack mod should be an integer")
-        self._damage_mod = damage_mod
+        self._attack_mod = attack_mod
         if not isinstance(damage_mod, int):
             raise ValueError("Damage mod should be an integer")
+        self._damage_mod = damage_mod
+        if not isinstance(damage_type, str):
+            raise ValueError("Damage type should be a string")
+        self._damage_type = damage_type
         self._damage_dice = validate_dice(damage_dice)
+        if not isinstance(name, str):
+            raise ValueError("Name should be a string")
         self._name = name
         if not isinstance(name, str):
             raise ValueError("Name should be a string")
@@ -24,6 +29,9 @@ class Attack:
 
     def get_damage_mod(self):
         return self._damage_mod
+
+    def get_damage_type(self):
+        return self._damage_type
 
     def get_damage_dice(self):
         return self._damage_dice
