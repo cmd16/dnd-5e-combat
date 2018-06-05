498ee3fc4229e7b6a76ed5733021495ebb052034
cmd16 <catherine.dejager10@gmail.com>
2018-06-05 00:50:23 -0400

    Write and test SavingThrowSpell

--- attack_class.py
+++ attack_class.py
@@ -3,41 +3,53 @@ from utility_methods_dnd import roll_dice, validate_dice, calc_advantage, time_t
 import weapons
 
 class Attack:
-    def __init__(self, damage_dice=None, attack_mod=0, damage_mod=0, damage_type="", range=0, melee_range=0, adv=0, name="",
-                 weapon=None, copy=None):
+    """damage_dice=None, attack_mod=0, damage_mod=0, damage_type="", range=0, melee_range=0, adv=0, name="",
+                 weapon=None, copy=None"""
+    def __init__(self, **kwargs):
+        # print("init Attack")
+        name = kwargs.get("name")
+        copy = kwargs.get("copy")
         if copy:
             self.copy_constructor(other=copy, name=name)
             return
-
-        self._damage_dice = validate_dice(damage_dice)  # providing no dice will be handled by validate_dice
+        self._damage_dice = validate_dice(kwargs.get("damage_dice"))  # providing no dice will be handled by validate_dice
+        attack_mod = kwargs.get("attack_mod", 0)
         if not isinstance(attack_mod, int):
             raise ValueError("Attack mod should be an integer")
         self._attack_mod = attack_mod
+        damage_mod = kwargs.get("damage_mod", 0)
         if not isinstance(damage_mod, int):
             raise ValueError("Damage mod should be an integer")
         self._damage_mod = damage_mod
+        damage_type = kwargs.get("damage_type", "")
         if not isinstance(damage_type, str):
             raise ValueError("Damage type should be a string")
         self._damage_type = damage_type
         if not isinstance(name, str):
             raise ValueError("Name should be a string")
-        if isinstance(range, int):
+        range = kwargs.get("range", 0)
+        if isinstance(range, int):  # TODO: handle various formats of range
             self._range = range
         else:  # TODO: give a warning?
-            self._range = 0
+            warnings.warn("Non-integer value for range provided")
+            self._range = range
+        melee_range = kwargs.get("melee_range", 0)
         if isinstance(melee_range, int):
             self._melee_range = melee_range
         else:
             self._melee_range = 0
         if not self._range and not self._melee_range:  # assume this is a melee attack if not otherwise specified
             self._melee_range = 5
+        adv = kwargs.get("adv", 0)
         if adv in [-1, 0, 1]:
             self._adv = adv
         else:
             raise ValueError("Advantage must be -1, 0, or 1")
+        name = kwargs.get("name")
         self._name = name
         if not isinstance(name, str):
             raise ValueError("Name should be a string")
+        weapon = kwargs.get("weapon")
         if isinstance(weapon, weapons.Weapon):
             self._weapon = weapon
         elif weapon:
@@ -131,21 +143,27 @@ class Attack:
         target.take_damage(damage)
 
 class SavingThrowAttack(Attack):
-    def __init__(self, copy=None, damage_dice=None, dc=None, save_type="", damage_on_success=False,
-                 damage_mod=0, damage_type="", range=0, melee_range=0, adv=0, name="", weapon=None):
+    """copy=None, damage_dice=None, dc=None, save_type="", damage_on_success=False,
+                 damage_mod=0, damage_type="", range=0, melee_range=0, adv=0, name="", weapon=None"""
+    def __init__(self, **kwargs):
+        # print("init SavingThrowAttack")
+        name = kwargs.get("name")
+        copy = kwargs.get("copy")
         if copy:
             self.copy_constructor(other=copy, name=name)
             return
 
-        super().__init__(damage_dice=damage_dice, damage_mod=damage_mod, damage_type=damage_type, range=range,
-                         melee_range=melee_range, adv=adv, name=name, weapon=weapon)
+        super().__init__(**kwargs)  # irrelevant keyword arguments will be ignored
+        dc = kwargs.get("dc")
         if not dc or not isinstance(dc, int):
             raise ValueError("Must provide DC (an int)")
         self._dc = dc
+        save_type = kwargs.get("save_type")
         if save_type in ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]:
             self._save_type = save_type
         else:
             raise ValueError("Save type must be strength, dexterity, constitution, intelligence, wisdom, or charisma")
+        damage_on_success = kwargs.get("damage_on_success")
         self._damage_on_success = damage_on_success  # half damage on successful save
 
     def copy_constructor(self, other, name=""):
@@ -195,23 +213,30 @@ class SavingThrowAttack(Attack):
             target.take_damage(damage//2)
 
 class Spell(Attack):
-    def __init__(self, level=None, school=None, casting_time=None, components=None, duration=None,  damage_dice=None,
+    """level=None, school=None, casting_time=None, components=None, duration=None,  damage_dice=None,
                  attack_mod=0, damage_mod=0, damage_type="", range=0, melee_range=0, adv=0, name="",
-                 copy=None):
+                 copy=None"""
+    def __init__(self, **kwargs):
+        # print("init Spell")
+        copy = kwargs.get("copy")
+        name = kwargs.get("name")
         if copy:
             self.copy_constructor(copy, name)
             return
-        super().__init__(damage_dice=damage_dice, attack_mod=attack_mod, damage_mod=damage_mod,
-                         damage_type=damage_type, range=range, melee_range=melee_range, adv=adv, name=name)
+        super().__init__(**kwargs)
+        level = kwargs.get("level")
         if isinstance(level, int) and 0 <= level < 10:
             self._level = level
         else:
             raise ValueError("Level must be an integer between 0 and 9")
+        school = kwargs.get("school")
         self._school = school  # TODO: validation
+        casting_time = kwargs.get("casting_time")
         if casting_time == "1 action" or casting_time == "1 bonus action" or casting_time == "1 reaction":
             self._casting_time = casting_time
         else:
             self._casting_time = time_to_rounds(casting_time)
+        components = kwargs.get("components")
         if not isinstance(components, (list, tuple)):
             raise ValueError("Components must be a list or tuple")
         self._components = []
@@ -221,6 +246,7 @@ class Spell(Attack):
             self._components.append("s")
         if "m" in components:
             self._components.append("m")
+        duration = kwargs.get("duration")
         self._duration = duration  # TODO: validation
 
     def copy_constructor(self, other, name=""):
@@ -250,9 +276,16 @@ class Spell(Attack):
         if level is None:
             level = self._level
         if source.get_level_spell_slots(level) or level == 0:
-            source.spend_slot(level)
+            source.spend_slot(level)  # TODO: check cantrip logic
         else:
             if source.get_verbose():
                 print("%s tried to cast a level %d spell even though they have no slots left for it" % (source.get_name(), level))
             return False
         super().make_attack(source=source, target=target, adv=adv)  # To be overridden in subclasses
+
+class SavingThrowSpell(Spell, SavingThrowAttack):
+    """level=None, school=None, casting_time=None, components=None, duration=None,
+                 dc=None, save_type="", damage_on_success=False, damage_dice=None, damage_mod=0,
+                 damage_type="", range=0, melee_range=0, adv=0, name="", copy=None"""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
